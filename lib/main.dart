489547d4262515e83e396f3dcd229b292320ba7d
surya2003-real <sgoel1811@gmail.com>
2023-09-04 23:02:34 +0530

    Image lists from API generated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'package:flutter_app_2/model/data_model.dart';
+//https://codeforgeek.com/flutter-fetch-data-from-rest-api/
 void main() => runApp(const MaterialApp(
   home: ImageDisplay(),
 ));
@@ -13,18 +16,40 @@ class ImageDisplay extends StatefulWidget {
 
 class _ImageDisplayState extends State<ImageDisplay> {
 
-  int ninjaLevel = 0;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  DataModel? dataFromAPI;
+  _getData() async {
+    try {
+      String url = "https://dummyjson.com/products";
+      http.Response res = await http.get(Uri.parse(url));
+      if (res.statusCode == 200) {
+        dataFromAPI = DataModel.fromJson(json.decode(res.body));
+        _isLoading = false;
+        setState(() {});
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
   String img_url1='https://docs.flutter.dev/assets/images/dash/dash-fainting.gif';
   String img_url2='https://i.imgur.com/Gi9cwgR.gif';
   String img_url='https://docs.flutter.dev/assets/images/dash/dash-fainting.gif';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.green[50],
       appBar: AppBar(
         title: const Text('Image Generator'),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
+        backgroundColor: Colors.green,
         elevation: 0.0,
       ),
       floatingActionButton: FloatingActionButton(
@@ -41,14 +66,29 @@ class _ImageDisplayState extends State<ImageDisplay> {
         backgroundColor: Colors.grey[800],
         child: Icon(Icons.add),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Image.network(img_url
+      body:  _isLoading
+          ? const Center(
+        child: CircularProgressIndicator(),
+      )
+          : ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.network(
+                  dataFromAPI!.products[index].thumbnail,
+                  width: 100,
+                ),
+                Text(dataFromAPI!.products[index].title.toString()),
+                Text("\$${dataFromAPI!.products[index].price.toString()}"),
+              ],
             ),
-          ),
-          const Text("Tap on the floating button to change gif"),
-        ],
+          );
+        },
+        itemCount: dataFromAPI!.products.length,
       ),
 
     );
